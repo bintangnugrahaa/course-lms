f13b1aaae36713763ac104222e0d30ef51d32cb5
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-12-27 19:09:41 +0700

    chore: update index.html title and favicon, enhance CSS styles, and add routing for sign-in and sign-up pages

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManagerHome from "../pages/ManagerHome/Index";
+import SignInPage from "../pages/SignIn";
+import SignUpPage from "../pages/SignUp";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ManagerHome />
-  }
-])
+    element: <ManagerHome />,
+  },
+  {
+    path: "/manager/sign-in",
+    element: <SignInPage />,
+  },
+  {
+    path: "/manager/sign-up",
+    element: <SignUpPage />,
+  },
+]);
 
-export default router
+export default router;
