ed8cb0c8b3d9e62ccd0ca929d6b846d84a0188e1
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-12-29 11:34:35 +0700

    feat: implement ManagerHome layout with Header, Sidebar, Course, and Student components, add new assets

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+export default function LayoutDashboard() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  )
+}
--- frontend/src/router/Index.jsx
+++ frontend/src/router/Index.jsx
@@ -3,6 +3,7 @@ import ManagerHome from "../pages/ManagerHome/Index";
 import SignInPage from "../pages/SignIn/Index";
 import SignUpPage from "../pages/SignUp/Index";
 import SuccessCheckoutPage from "../pages/SuccessCheckout/Index";
+import LayoutDashboard from "../components/Layout";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,16 @@ const router = createBrowserRouter([
   {
     path: "/success-checkout",
     element: <SuccessCheckoutPage />
+  },
+  {
+    path: "/manager",
+    element: <LayoutDashboard />,
+    children: [
+      {
+        index: true,
+        element: <ManagerHome />
+      }
+    ]
   }
 ])
 
