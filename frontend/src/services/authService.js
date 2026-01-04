dcd3e8c19e31ba13ab5b964203c115d5620a4f02
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-01-05 00:32:12 +0700

    feat: implement sign-in page with form validation using React Hook Form and Zod, integrate secure local storage for user session management

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,3 +2,5 @@ import apiInstance from "../utils/axios";
 
 export const postSignUp = async (data) =>
   apiInstance.post("/sign-up", data).then((res) => res.data);
+export const postSignIn = async (data) =>
+  apiInstance.post("/sign-in", data).then((res) => res.data);
--- frontend/src/utils/zodSchema.js
+++ frontend/src/utils/zodSchema.js
@@ -1,11 +1,9 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
-  name: z.string().min(5, { message: "Name must be at least 5 characters" }),
-
-  email: z.string().email({ message: "Please enter a valid email address" }),
-
-  password: z
-    .string()
-    .min(5, { message: "Password must be at least 5 characters" }),
+  name: z.string().min(5, "Name too short"),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(5, "Password too short"),
 });
+
+export const signInSchema = signUpSchema.omit({ name: true });
