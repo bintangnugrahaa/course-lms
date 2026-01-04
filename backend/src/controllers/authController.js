4519bb7d64551e61f31f91b77e4fe26be7fbd68b
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-01-04 12:33:48 +0700

    feat: add sign-in functionality with JWT authentication, including user validation and error handling

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import transactionModel from "../models/transactionModel.js";
+import jwt from "jsonwebtoken";
 
 export const signUpAction = async (req, res) => {
   const midtransUrl = process.env.MIDTRANS_URL;
@@ -56,6 +57,70 @@ export const signUpAction = async (req, res) => {
       message: "Sign Up Success",
       data: {
         midtrans_payment_url: resMidtrans.redirect_url,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const signInAction = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const existingUser = await userModel
+      .findOne()
+      .where("email")
+      .equals(body.email);
+
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    const comparePassword = bcrypt.compareSync(
+      body.password,
+      existingUser.password
+    );
+
+    if (!comparePassword) {
+      return res.status(400).json({
+        message: "Email / password incorrect",
+      });
+    }
+
+    const isValidUser = await transactionModel.findOne({
+      user: existingUser._id,
+      status: "success",
+    });
+
+    if (existingUser.role !== "student" && !isValidUser) {
+      return res.status(400).json({
+        message: "User not verified",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        data: {
+          id: existingUser._id.toString(),
+        },
+      },
+      process.env.SECRET_KEY_JWT,
+      { expiresIn: "1 days" }
+    );
+
+    return res.json({
+      message: "User Logged in success",
+      data: {
+        name: existingUser.name,
+        email: existingUser.email,
+        token,
+        role: existingUser.role,
       },
     });
   } catch (error) {
