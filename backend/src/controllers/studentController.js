208c1067725f643e63c0cde6ff6cbcec7bf24f86
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-01-17 14:14:10 +0700

    feat: implement student creation functionality with validation and error handling in the student controller

--- backend/src/controllers/studentController.js
+++ backend/src/controllers/studentController.js
@@ -1,4 +1,10 @@
 import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import fs from "fs";
+import { mutateStudentSchema } from "../utils/schema.js";
+
+const serverError = (res) =>
+  res.status(500).json({ message: "Internal server error" });
 
 export const getStudents = async (req, res) => {
   try {
@@ -11,7 +17,43 @@ export const getStudents = async (req, res) => {
       message: "Get Students success",
       data: students,
     });
-  } catch (error) {
+  } catch {
+    return serverError(res);
+  }
+};
+
+export const postStudent = async (req, res) => {
+  try {
+    const parse = mutateStudentSchema.safeParse(req.body);
+
+    if (!parse.success) {
+      if (req?.file?.path && fs.existsSync(req.file.path)) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      return res.status(400).json({
+        message: "Validation error",
+        errors: parse.error.issues.map((err) => err.message),
+      });
+    }
+
+    const hashPassword = bcrypt.hashSync(parse.data.password, 12);
+
+    const student = new userModel({
+      name: parse.data.name,
+      email: parse.data.email,
+      password: hashPassword,
+      photo: req.file?.filename,
+      manager: req.user._id,
+      role: "student",
+    });
+
+    await student.save();
+
+    return res.json({
+      message: "Create student success",
+    });
+  } catch {
     return serverError(res);
   }
 };
--- backend/src/utils/schema.js
+++ backend/src/utils/schema.js
@@ -26,3 +26,9 @@ export const mutateContentSchema = z.object({
   text: z.string().optional(),
   courseId: z.string().min(5, { message: "Invalid course." }),
 });
+
+export const mutateStudentSchema = z.object({
+  name: z.string().min(5, { message: "Minimum 5 characters." }),
+  email: z.string().email({ message: "Invalid email." }),
+  password: z.string().min(8, { message: "Minimum 8 characters." }),
+});
